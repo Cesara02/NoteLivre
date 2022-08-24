e7c8edb46c603b77163ad9f3816ebb5e3b288828
Cesara02 <alexiscsr26@icloud.com>
2022-08-25 01:29:20 +0200

    Mecanisme session

---
+++ session.php
@@ -0,0 +1,47 @@
+<?php session_start();
+
+include("./class/User.php");
+
+$TheUser = new User(null, null, null);
+
+    try {
+        $ipserver = "localhost";
+        $nomBase = "NoteLivre";
+        $loginPrivilege = "Alexis";
+        $passwordPrivilege = "Alexis";
+
+        $GLOBALS["pdo"] = new PDO ('mysql:host='.$ipserver.';dbname='.$nomBase.'', $loginPrivilege, $passwordPrivilege);
+        // echo "Connexion à la base de donnée réussi ! ";
+    } catch (Exception $e) {
+        $e->getMessage();
+    }
+
+    if(isset($_POST['Connexion'])) {
+        $TheUser->seConnecter($_POST['login'], $_POST['password']);
+    }
+
+    if(isset($_POST['Deconnexion'])) {
+        // echo "Vous êtes déconnectez";
+        session_unset();
+        session_destroy();
+    }
+
+    if(isset($_SESSION['Connexion']) && ($_SESSION['Connexion'] == true)) {
+        echo "Vous êtes déjà connecté !";
+        ?>
+        <form action = "" method = "POST">
+            <input type = "submit" name = "Deconnexion" value = "Se deconnecter">
+        </form>
+        <a href = "page2.php"> 🚪 Accès à la page 2 </a>
+        <?php
+    } else {
+        echo "Veuillez vous identifier...";
+        ?>
+        <form action = "" method = "POST">
+            Login : <input type = "text" name = "login" value = "Alexis">
+            Password : <input type = "password" name = "password" value = "Alexis">
+            <input type = "submit" name = "Connexion" value = "Se connecter">
+        </form>
+        <?php
+    }
+?>
